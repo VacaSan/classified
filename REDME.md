b0a29d010a91f0c8caa4ee82e27ea80faa413b89
VacaSan <kotaracvladan@gmail.com>
2020-08-16 12:54:38 +0200

    initial commit

---
+++ REDME.md
@@ -0,0 +1,3 @@
+# classified
+
+// TODO
---
+++ src/classified.js
@@ -0,0 +1,38 @@
+import React from "react";
+import curryN from "ramda/src/curryN";
+import isPropValid from "@emotion/is-prop-valid";
+
+const isFunc = x => typeof x === "function";
+
+function validateProps(props = {}) {
+  const validProps = {};
+  for (const prop in props) {
+    if (props.hasOwnProperty(prop) && isPropValid(prop)) {
+      validProps[prop] = props[prop];
+    }
+  }
+  return validProps;
+}
+
+const classified = curryN(2, function classified(tag, names = []) {
+  // ? className = staticNames + dynamicNames(props) + props.className
+  return React.forwardRef(function Classified({ as: T = tag, ...props }, ref) {
+    // ? can we optimize this
+    const className = Array.prototype
+      .concat(names, props.className)
+      .map(name => (isFunc(name) ? name(props) : name))
+      .filter(Boolean)
+      .join(" ");
+
+    // ! only validate for html elements
+    const elementProps = validateProps(props);
+
+    return <T {...elementProps} className={className} ref={ref} />;
+  });
+});
+
+function cx(names = []) {
+  return names.filter(Boolean).join(" ");
+}
+
+export { classified, cx };
