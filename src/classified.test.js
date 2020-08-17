df09453c7cd3b4081195e7af165a690620a2ba38
VacaSan <kotaracvladan@gmail.com>
2020-08-17 14:56:41 +0200

    test: ref + as prop

--- src/classified.test.js
+++ src/classified.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
+import { findDOMNode } from "react-dom";
+import { renderIntoDocument } from "react-dom/test-utils";
 import { classified } from "./classified";
 
 test("it should inject className", () => {
@@ -12,4 +14,40 @@ test("it should pass props to component", () => {
   const Comp = classified("div", []);
   const wrapper = TestRenderer.create(<Comp onClick={() => null} />);
   expect(wrapper.root.findByType("div").props.onClick).toBeDefined();
+});
+
+test("it should pass the ref to the component", () => {
+  const Comp = classified("div", ["test-class"]);
+
+  class Wrapper extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.testRef = React.createRef();
+    }
+
+    render() {
+      return (
+        <div>
+          <Comp ref={this.testRef} />
+        </div>
+      );
+    }
+  }
+
+  const wrapper = renderIntoDocument(<Wrapper />);
+
+  const component = findDOMNode(wrapper).querySelector(".test-class");
+
+  expect(wrapper.testRef.current).toBe(component);
+});
+
+test("as prop changes the rendered element type", () => {
+  const Comp = classified("button", ["btn"]);
+  const wrapper = TestRenderer.create(<Comp as="a" />);
+  expect(wrapper.toJSON()).toMatchInlineSnapshot(`
+    <a
+      className="btn"
+    />
+  `);
 });
